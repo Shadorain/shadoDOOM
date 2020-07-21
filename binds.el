edb5ab150d6ebb5beb05aac5bb06cc372757ce71
Shadorain <jordanf7517@gmail.com>
2020-07-20 23:47:59 -0400

    FINALLY FIXED EVERYTHING, i know what all my issues with gui are aswell and i finally got my literate config up and perfect :DD

--- binds.el
+++ binds.el
@@ -1,5 +1,8 @@
 ;;; ~/.doom.d/binds.el -*- lexical-binding: t; -*-
 
+;; Super key instead of Alt
+(setq x-super-keysym 'meta)
+
 ;; Clipboard paste
 (global-set-key (kbd "C-S-v") #'clipboard-yank)
 
